dd5cdc680e20edb7efcb0b6b0c10bb72d3f31d4b
Jurica Šeparović <jsepar00@fesb.hr>
2019-02-11 14:40:14 +0100

    Componentize swiper slide

---
+++ resources/views/components/slide.blade.php
@@ -0,0 +1,9 @@
+<div class="swiper-slide">
+    <div
+        class="slide__hero border-brand absolute z-20 pin-x pin-y flex items-center ml-8 lg:ml-32 leading-loose">
+        <div class="flex flex-col text-center w-84 -mt-5">
+            {{ $slot }}
+        </div>
+    </div>
+    <img class="image-bg" src="{{ $image }}"/>
+</div>
--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,46 +1,30 @@
 <section class="h-full m-0 p-0">
     <div class="hidden md:block swiper-container h-screen border-b">
         <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <div
-                    class="slide__hero border-brand absolute z-20 pin-x pin-y flex items-center ml-8 lg:ml-32 leading-loose">
-                    <div class="flex flex-col text-center w-84 -mt-5">
-                        @include('partials.hero', [
-                            'title' => 'Pakleni islands',
-                            'shortDescription' => 'Feel the vibe',
-                            'description' => ' Sailing around the labyrinth of Pakleni islands! Find hidden beautiful beaches and deserted lagoons',
-                            'callToAction' => "LET'S GO"
-                        ])
-                    </div>
-                </div>
-                <img class="image-bg" src="{{ asset('images/header/tour.jpg') }}"/>
-            </div>
-            <div class="swiper-slide bg-grey-darkest">
-                <div class="slide__hero border-brand absolute z-20 pin-x pin-y flex items-center ml-32 leading-loose">
-                    <div class="flex flex-col text-center w-84 -mt-5">
-                        @include('partials.hero', [
-                           'title' => 'Pakleni islands',
-                           'shortDescription' => 'Feel the vibe',
-                           'description' => ' Sailing around the labyrinth of Pakleni islands! Find hidden beautiful beaches and deserted lagoons',
-                           'callToAction' => "LET'S GO"
-                       ])
-                    </div>
-                </div>
-                <img class="image-bg" src="{{ asset('images/header/boat.jpg') }}"/>
-            </div>
-            <div class="swiper-slide bg-grey-darkest">
-                <div class="slide__hero border-brand absolute z-20 pin-x pin-y flex items-center ml-32 leading-loose">
-                    <div class="flex flex-col text-center w-84 -mt-5">
-                        @include('partials.hero', [
-                            'title' => 'Pakleni islands',
-                            'shortDescription' => 'Feel the vibe',
-                            'description' => ' Sailing around the labyrinth of Pakleni islands! Find hidden beautiful beaches and deserted lagoons',
-                            'callToAction' => "LET'S GO"
-                        ])
-                    </div>
-                </div>
-                <img class="image-bg" src="{{ asset('images/header/zrak.jpg') }}"/>
-            </div>
+            @component('components.slide', ['image' => asset('images/header/boat.jpg')])
+                @include('partials.hero', [
+                   'title' => 'Pakleni islands',
+                   'shortDescription' => 'Feel the vibe',
+                   'description' => ' Sailing around the labyrinth of Pakleni islands! Find hidden beautiful beaches and deserted lagoons',
+                   'callToAction' => "LET'S GO"
+               ])
+            @endcomponent
+            @component('components.slide', ['image' => asset('images/header/zrak.jpg')])
+                @include('partials.hero', [
+                   'title' => 'Pakleni islands',
+                   'shortDescription' => 'Feel the vibe',
+                   'description' => ' Sailing around the labyrinth of Pakleni islands! Find hidden beautiful beaches and deserted lagoons',
+                   'callToAction' => "LET'S GO"
+               ])
+            @endcomponent
+            @component('components.slide', ['image' => asset('images/header/tour.jpg')])
+                @include('partials.hero', [
+                   'title' => 'Pakleni islands',
+                   'shortDescription' => 'Feel the vibe',
+                   'description' => ' Sailing around the labyrinth of Pakleni islands! Find hidden beautiful beaches and deserted lagoons',
+                   'callToAction' => "LET'S GO"
+               ])
+            @endcomponent
         </div>
         <div class="slider-control -mt-12">
             <div class="slider-control--next mb-2 px-4 flex flex-col justify-center items-center">
