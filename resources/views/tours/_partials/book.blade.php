7c6de3773a92b055f695cd72cbd13c58963fb2de
Jurica Šeparović <jsepar00@fesb.hr>
2019-02-19 17:25:12 +0100

    Finish single tour listing

---
+++ resources/views/tours/_partials/book.blade.php
@@ -0,0 +1,7 @@
+<div class="lg:sticky sticky-book bg-white p-10 mt-10">
+    <h1 class="py-3 text-xl leading-tight">Book</h1>
+    <p class="text-grey mb-2">Send us inquiry to verify that trip is available on your desired dates.</p>
+    <send-inquiry action="{{ route('tour.request') }}"></send-inquiry>
+</div>
+
+
