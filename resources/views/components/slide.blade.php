28b6cb988a366c1635c48e1964072a2cb31ccba5
Jurica Šeparović <jsepar00@fesb.hr>
2019-03-12 20:43:03 +0100

    Fix nav and title

--- resources/views/components/slide.blade.php
+++ resources/views/components/slide.blade.php
@@ -1,7 +1,7 @@
 <div class="swiper-slide z-50">
     <div
         class="container max-w-4xl px-12 slide__hero border-brand absolute z-20 pin-x pin-y flex items-center leading-loose">
-        <div class="flex flex-col text-center w-84 -mt-5 z-10 xl:ml-5">
+        <div class="flex flex-col text-center w-84 z-10">
             {{ $slot }}
         </div>
     </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav
     class="navigation text-center pin-t pin-r pin-l z-20 {{ isset($fixedOff) ? 'bg-black' : 'absolute'}}">
-    <div class="flex justify-between items-center relative my-2 lg:my-0 max-w-4xl px-4 mx-auto">
+    <div class="flex justify-between items-center relative my-2 lg:my-0 max-w-4xl px-4 xl:px-0 mx-auto">
         <div>
             <a href="/" class="link-reset">
                 <span class="text-brand font-bold text-2xl">Revenger</span>
