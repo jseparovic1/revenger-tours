d715ab3a4bcfe30d2eaba4a6bf4f65153a668bb5
Jurica Šeparović <jsepar00@fesb.hr>
2019-03-14 22:25:55 +0100

    Add favicons and social tags

---
+++ resources/views/layouts/_social.blade.php
@@ -0,0 +1,6 @@
+<meta property="og:image" content="{{ asset('images/og-image.jpg') }}">
+<meta property="og:image:width" content="279">
+<meta property="og:image:height" content="279">
+<meta property="og:title" content="Revenger tours">
+<meta property="og:description" content="Revenger tours is travel agency from Split  specialized in  daily excursions,as well as sea transfers from Split to nearby islands.">
+<meta property="og:url" content="https://revengertours.com">
