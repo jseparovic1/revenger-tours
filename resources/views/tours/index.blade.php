d7e4ec91ba5642f1c670654d6d5baf92c9ec7360
Jurica Šeparović <jsepar00@fesb.hr>
2019-03-14 22:00:27 +0100

    Fix show listing

--- resources/views/tours/index.blade.php
+++ resources/views/tours/index.blade.php
@@ -15,7 +15,7 @@
         <div class="px-2">
             <div class="flex flex-wrap -mx-2">
                 @foreach($tours as $tour)
-                    <a href="{{ route('tours.show', $tour->slug) }}" class="w-full max-w-xl lg:w-1/2 px-2 flex-no-shrink mb-8 mx-auto">
+                    <a href="{{ route('tours.show', $tour->slug) }}" class="w-full max-w-xl lg:w-1/2 px-8 flex-no-shrink mb-10 mx-auto">
                         <div class="h-full flex flex-col sm:flex-row justify-between hover:shadow mb-10">
                             <div class="w-full flex img-zoom-wrapper">
                                 <img
@@ -23,10 +23,10 @@
                                     src="{{ optional($tour->getFirstMedia('hero_original'))->getUrl('card') ?? asset('images/default/card.jpg')}}"
                                 />
                             </div>
-                            <div class="w-full lg:w-5/6 flex flex-col justify-around px-5 pb-5">
-                                <h1 class="text-xl font-bold text-black mb-2">{{ $tour->title }}</h1>
-                                <p class="text-lg mb-4 text-grey-darker">{{ str_limit($tour->short_description, 100, '...') }}</p>
-                                <button class="btn btn-primary rounded-full">BOOK NOW</button>
+                            <div class="w-full lg:w-5/6 flex flex-col justify-around px-5">
+                                <h1 class="text-lg font-bold text-black mb-2">{{ $tour->title }}</h1>
+                                <p class="mb-4 text-grey-darker">{{ str_limit($tour->short_description, 100, '...') }}</p>
+                                <button class="btn btn-primary rounded-full">BOOK</button>
                             </div>
                         </div>
                     </a>
