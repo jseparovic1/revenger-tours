7da3389e9ebaf5e1812c94d35aa81709c4c7e0a1
Jurica Šeparović <jsepar00@fesb.hr>
2019-03-29 14:19:27 +0100

    Remove blurry transition from tour

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -5,7 +5,7 @@
             <p class="heading-description">{{ config('settings.main_blog.description') }}</p>
             <div class="featured flex flex-row flex-wrap md:flex-no-wrap -mx-4 self-stretch mb-4">
                 @foreach($posts as $post)
-                    <div class="tour w-full mx-4 flex flex-col md:shadow mb-4 md:mb-0 rounded-lg overflow-hidden">
+                    <div class="tour w-full mx-4 flex flex-col md:shadow mb-4 md:mb-0 rounded-lg overflow-hidden group">
                         <img class="h-auto w-full bg-cover"
                              alt="{{ $post->title . ' image' }}"
                              src="{{ $post->getFirstMediaUrl(\App\Post::COVER_COLLECTION) }}"
@@ -13,7 +13,7 @@
                         <a class="flex flex-col h-full justify-around p-5 hover:text-brand rounded-b-lg" href="{{ $post->slug }}">
                             <h1 class="text-lg font-semibold text-black mb-2">{{ $post->title }}</h1>
                             <p class="text-grey-darkest">{{ $post->description }}</p>
-                            <a class="text-grey text-sm p-5" href="{{ $post->slug }}">READ MORE</a>
+                            <a class="text-grey text-sm p-5 group-hover:text-brand" href="{{ $post->slug }}">READ MORE</a>
                         </a>
                     </div>
                 @endforeach
--- resources/views/home/featured/tour.blade.php
+++ resources/views/home/featured/tour.blade.php
@@ -1,4 +1,4 @@
-<div class="tour w-full mx-4 flex flex-col md:shadow mb-4 md:mb-0 rounded-lg overflow-hidden">
+<div class="group tour w-full mx-4 flex flex-col md:shadow mb-4 md:mb-0 rounded-lg overflow-hidden">
     <img class="h-auto w-full bg-cover"
          alt="{{ $title . ' image' }}"
          @if(is_string($media))
@@ -7,9 +7,12 @@
          src="{{ $media->getUrl('card') }}"
          @endif
     />
-    <a class="flex flex-col h-full justify-around p-5 hover:text-brand rounded-b-lg" href="{{ $action }}">
+    <a class="flex flex-col h-full justify-around p-5 rounded-b-lg" href="{{ $action }}">
         <h1 class="text-lg font-semibold text-black mb-2">{{ $title }}</h1>
         <p class="mb-4 text-grey-darkest">{{ $description }}</p>
-        <a class="btn btn-primary link-reset hover:text-white text-center rounded-none" href="{{ $action }}">SEE MORE</a>
+        <a class="btn btn-primary link-reset group-hover:text-white text-center rounded-none group-hover:bg-brand-darker"
+           href="{{ $action }}">
+            SEE MORE
+        </a>
     </a>
 </div>
