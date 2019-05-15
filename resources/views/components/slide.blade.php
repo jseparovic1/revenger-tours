09136c3dc193d2a17fe951ede0e7415b91634bc1
Jurica Šeparović <jsepar00@fesb.hr>
2019-05-15 22:00:03 +0200

    Remove dot and add analytics to .env example

--- resources/views/components/slide.blade.php
+++ resources/views/components/slide.blade.php
@@ -1,4 +1,4 @@
-<div class="swiper-slide z-50 relative" style="background-image: url(.{{$image}}); background-size: cover; background-position: center;">
+<div class="swiper-slide z-50 relative" style="background-image: url({{$image}}); background-size: cover; background-position: center;">
     <div
         class="container max-w-3xl px-12 slide__hero border-brand absolute z-20 pin-x pin-y flex items-center leading-loose">
         <div class="flex flex-col text-center w-84 z-10">
