564ec57b417b0d00d6061f4240e4cd1b2393c53f
jseparovic1 <jurica.separovic@gmail.com>
2020-03-02 18:52:50 +0100

    Remove sluggable.

--- app/Post.php
+++ app/Post.php
@@ -6,27 +6,16 @@ use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 use Spatie\MediaLibrary\Models\Media;
-use Spatie\Sluggable\HasSlug;
-use Spatie\Sluggable\SlugOptions;
 
 class Post extends Model implements HasMedia
 {
-    use HasMediaTrait, HasSlug;
+    use HasMediaTrait;
 
     const COVER_COLLECTION = 'cover';
 
     public function getRouteKeyName()
     {
         return 'slug';
-    }
-
-    public function getSlugOptions(): SlugOptions
-    {
-        return SlugOptions::create()
-            ->generateSlugsFrom('title')
-            ->saveSlugsTo('slug')
-            ->doNotGenerateSlugsOnUpdate()
-            ->slugsShouldBeNoLongerThan(20);
     }
 
     public function registerMediaCollections()
